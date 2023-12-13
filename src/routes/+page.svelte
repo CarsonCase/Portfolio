68ea535a78c6f73fd0117561485e3685b110b587
Carson Case <carsoncase@gmail.com>
2023-12-12 17:32:39 -0800

    more path stuff...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,7 +28,7 @@
                 
 
                     <div class="flex justify-center">
-                        <img class="h-[300px]" src="Headshot.jpg" alt="">
+                        <img class="h-[300px]" src="assets/Headshot.jpg" alt="">
                     </div>
 
                 </div>
--- src/routes/DesktopIcon.svelte
+++ src/routes/DesktopIcon.svelte
@@ -2,7 +2,7 @@
     import Window from "./Window.svelte";
     import {base} from "$app/paths";
 
-    const PATH = "icons/"
+    const PATH = "assets/icons/"
     export let src = "Windows 7 Folder.png"
     export let display = false
     export let text = "My Folder"
