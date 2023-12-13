5d01cb85b22e22f6c05a74b0253bbec01fbc83ad
Carson Case <carsoncase@gmail.com>
2023-12-12 16:43:09 -0800

    removed .vercel

--- .vercel/output/functions/fn-0.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.c7605e70.js","_app/immutable/chunks/scheduler.a629d90a.js","_app/immutable/chunks/index.0dcb5595.js"];
-export const stylesheets = ["_app/immutable/assets/0.506057c3.css"];
-export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/entries/pages/_layout.svelte.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const app = "";
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<link rel="stylesheet" href="https://unpkg.com/7.css"> ${slots.default ? slots.default({}) : ``}`;
-});
-export {
-  Layout as default
-};
